f0dd0a90b7b4db783a46bb15b2ad5f32f50b24ad
Linus <linus.schulze2001@gmail.com>
2025-05-13 16:59:27 +0200

    Swagger UI
    tested, simple queries run

--- backend/app.js
+++ backend/app.js
@@ -9,14 +9,64 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+//Swagger comments for docs
+/**
+ * @swagger
+ * /api/sciencedirect/search:
+ *   get:
+ *     summary: Suche in ScienceDirect
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Der Suchbegriff
+ *     responses:
+ *       200:
+ *         description: Erfolgreiche Antwort mit Suchergebnis
+ */
 app.get('/api/sciencedirect/search', (req, res) => {
-  res.json({ source: 'ScienceDirect', query: req.query.query });
-});
+    res.json({ source: 'ScienceDirect', query: req.query.query });
+  });
 
+
+/**
+ * @swagger
+ * /api/springer/search:
+ *   get:
+ *     summary: Suche in Springer Link
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Der Suchbegriff
+ *     responses:
+ *       200:
+ *         description: Erfolgreiche Antwort mit Suchergebnis
+ */
 app.get('/api/springer/search', (req, res) => {
-  res.json({ source: 'Springer', query: req.query.query });
-});
-
+    res.json({ source: 'SpringerLink', query: req.query.query });
+  });
+  
+/**
+ * @swagger
+ * /api/ais/search:
+ *   get:
+ *     summary: Suche in ais Link
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Der Suchbegriff
+ *     responses:
+ *       200:
+ *         description: Erfolgreiche Antwort mit Suchergebnis
+ */
 app.get('/api/ais/search', (req, res) => {
   res.json({ source: 'AIS eLibrary', query: req.query.query });
 });
