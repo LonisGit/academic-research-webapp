677079359228c90d0b1c1b9d33042bac884ec3cc
RafinUddin <131182466+RafinUddin@users.noreply.github.com>
2025-05-21 12:09:43 +0200

    api verbindung science direct

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-api_key = "8ecc623d9f81c2dd77928cb559377c83"
-#url = "https://api.elsevier.com/content/search/sciencedirect"
-url = "https://pokeapi.co/api/v2/pokemon/ditto"
-params = {"query": "machine learning", "apiKey": api_key}
-#response = requests.get(url, params=params)
-response = requests.get(url)
-data = response.json()
-print(data["name"])
